406e017c13d43f8a78895fe1f87759b30872da9e
PoojaPatel9 <prp25@njit.edu>
2025-02-08 21:16:07 -0500

    update test file

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,43 +1,35 @@
-'''My Calculator Test
-from calculator import add, subtract
-
-def test_addition():
-    Test that addition function works 
-    assert add(2, 2) == 4
-
-def test_subtraction():
-    Test that subtraction function works 
-    assert subtract(2, 2) == 0 '''
+# test_calculator.py
 
 import pytest
-from calculator.operations import Calculator
+from calculator import Calculator
 
-def test_add(setup_calculator):
-    calc = setup_calculator
-    result = calc.add(1, 2)  # example of a method on Calculator
-    assert result == 3
-def setup_calculator():
-    Calculator.clear_history()
-    return Calculator
+@pytest.fixture
+def calc():
+    return Calculator()
 
-def test_add(setup_calculator):
-    assert setup_calculator.operate("+", 3, 2) == 5
+def test_add(calc):
+    assert calc.add(2, 3) == 5
+    assert calc.history[-1] == "Added: 2 + 3 = 5"
 
-def test_subtract(setup_calculator):
-    assert setup_calculator.operate("-", 5, 2) == 3
+def test_subtract(calc):
+    assert calc.subtract(5, 3) == 2
+    assert calc.history[-1] == "Subtracted: 5 - 3 = 2"
 
-def test_multiply(setup_calculator):
-    assert setup_calculator.operate("*", 4, 3) == 12
+def test_multiply(calc):
+    assert calc.multiply(2, 3) == 6
+    assert calc.history[-1] == "Multiplied: 2 * 3 = 6"
 
-def test_divide(setup_calculator):
-    assert setup_calculator.operate("/", 8, 2) == 4
+def test_divide(calc):
+    assert calc.divide(6, 3) == 2
+    assert calc.history[-1] == "Divided: 6 / 3 = 2"
 
-def test_divide_by_zero(setup_calculator):
-    with pytest.raises(ZeroDivisionError):
-        setup_calculator.operate("/", 10, 0)
+def test_divide_by_zero(calc):
+    with pytest.raises(ValueError):
+        calc.divide(6, 0)
 
-def test_history_tracking(setup_calculator):
-    setup_calculator.operate("+", 3, 3)
-    setup_calculator.operate("*", 2, 2)
-    history = setup_calculator.get_history()
+def test_retrieve_history(calc):
+    calc.add(2, 3)
+    calc.subtract(5, 3)
+    history = calc.retrieve_history()
     assert len(history) == 2
+    assert history[0] == "Added: 2 + 3 = 5"
